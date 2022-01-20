67d81a9dc8b4f7283bd4ccc01a9cb331f86e7471
Nc Galih <encegalih@gmail.com>
2022-01-20 16:17:23 +0700

    init project next-app & tailwind

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,10 @@
 import '../styles/globals.css'
+import {useEffect} from 'react'
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    document.documentElement.style.setProperty('--vh',`${window.innerHeight * 0.01}px`)
+  })
   return <Component {...pageProps} />
 }
 
