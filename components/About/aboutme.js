13e1dc3f5d45151a50c55b3798e451b95fc1cecc
Nc Galih <encegalih@gmail.com>
2022-01-25 19:30:48 +0700

    fix bug

--- components/About/aboutme.js
+++ components/About/aboutme.js
@@ -1,8 +1,8 @@
-const Aboutme = ({title, about}) => {
+const Aboutme = ({title, desc}) => {
     return (
         <div className="max-w-2xl">
             <h1 className="text-gray-900 dark:text-white text-2xl mb-1.5">{title}</h1>
-            <p className="text-gray-700 dark:text-gray-200">{about}</p>
+            <p className="text-gray-700 dark:text-gray-200">{desc}</p>
         </div>
     )
 }
